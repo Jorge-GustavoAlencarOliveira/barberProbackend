fc057d59fd798f667f3b97e4067204e5b7d86762
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-04-13 23:16:34 -0300

    nova

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express();
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next:NextFunction) => {
   if (err instanceof Error){
