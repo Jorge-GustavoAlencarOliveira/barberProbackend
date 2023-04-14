9e84194ad222bc439fe666602d9a9bd047f78a45
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-04-13 22:58:53 -0300

    denovo

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { router } from './routes';
 import cors from 'cors'
 
 const app = express();
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next:NextFunction) => {
@@ -17,7 +17,6 @@ app.use((err: Error, req: Request, res: Response, next:NextFunction) => {
     status: 'error',
     message: 'Internal Server Error'
   })
-
 })
 
 app.listen(process.env.PORT || 3333, () => console.log('servidor online') )
